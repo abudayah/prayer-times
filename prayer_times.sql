ff1965340ae00891ae222ccb3c66f48c6282a4ee
Anas Abu dayah <anas.abudayah@gmail.com>
2025-03-08 14:21:40 -0800

    adding database dump

---
+++ prayer_times.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 07, 2025 at 04:56 AM
+-- Server version: 9.2.0
+-- PHP Version: 8.1.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `prayer_times`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posters`
+--
+
+CREATE TABLE `posters` (
+  `id` int NOT NULL,
+  `name` text NOT NULL,
+  `file_path` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `posters`
+--
+
+INSERT INTO `posters` (`id`, `name`, `file_path`, `timestamp`) VALUES
+(47, 'quran', 'uploads/67c9969eec040_quran-3.jpg', '2025-03-07 03:30:28'),
+(51, 'Ramadan', 'uploads/67c9c47a5873e_New Project (1).jpg', '2025-03-06 15:51:22'),
+(54, 'Ramadan kareem 2', 'uploads/67ca67a06ab77_ramadan-kareem.png', '2025-03-07 03:35:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'admin', '$2y$10$ROfhWj1qrNVOHPsfQIHeQuxeXeupxCpbD9KsfDhxAawsJjw7PqlSK');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `posters`
+--
+ALTER TABLE `posters`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `posters`
+--
+ALTER TABLE `posters`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
